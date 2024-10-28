3f9dabe67c532292ffc23e19ae5259b6cdff4a98
Владимир <vm885032@gmail.com>
2024-10-28 16:54:46 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,9 @@
+number_completed_hom_assign = 12
+number_hours_spent = 1.5
+course_name = "Python"
+time_for_one_task = number_hours_spent / number_completed_hom_assign
+
+print("Курс:", course_name + ",", "всего задач:", str(number_completed_hom_assign) + ",",
+"затрачено часов:",str(number_hours_spent) + ",", "среднее время выполнения:",
+time_for_one_task, "часа.")
+
