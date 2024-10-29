eaefd98aac0c708c8023ef862a85ebb0a94668fd
Владимир <vm885032@gmail.com>
2024-10-29 12:36:58 +0300

    Add files via upload

---
+++ inx_str.py
@@ -0,0 +1,12 @@
+exemple = "Vladimir"
+print(exemple[0])
+print(exemple[-1])
+print(exemple[5:])
+print(exemple[::-1])
+print(exemple[1:-1:2])
+print("#" * 30)
+exemple = "Топинамбур"
+print(exemple[0])
+print(exemple[-1])
+print(exemple[::-1])
+print(exemple[1:-1:2])
