44d5f1886af449c48d291a91dcbc6159c1444215
Владимир <vm885032@gmail.com>
2024-11-03 12:33:00 +0300

    Add files via upload

---
+++ modul_1_5.py
@@ -0,0 +1,12 @@
+
+
+immutable_var = 1, 2, "a", "b", [1, 2], True, False
+print(immutable_var)
+
+# immutable_var[0] = 4  # Кортеж не изменяемый тип данных.
+
+immutable_list = [1, 2, "a", "b", True, False]
+print(immutable_list)
+
+immutable_list[2] = "мохито"
+print(immutable_list)
