c01067d8c2a33d3888b192245456503b7a3e8e3f
Владимир <vm885032@gmail.com>
2024-11-25 12:09:45 +0300

    Add files via upload

--- additional_task.py
+++ additional_task.py
@@ -5,11 +5,29 @@ students_lst = list(frozenset(students)) # converting a set to a list
 
 students_lst.sort() # we arrange the names in alphabetical order
 
-dict_res = {students_lst[0]: sum(grades[0]) / len(grades[0]),
-            students_lst[1]: sum(grades[1]) / len(grades[1]),
-            students_lst[2]: sum(grades[2]) / len(grades[2]),
-            students_lst[3]: sum(grades[3]) / len(grades[3]),
-            students_lst[4]: sum(grades[4]) / len(grades[4])
-            }
+dict_res = {}
+average_scoresum = 0
+
+while True:
+    stds = str
+    grds = 0
+    grds_l = []
+    stds = input("Введите имя студента(0 - закончить ввод):")
+
+    while True:
+        grds = int(input("Введите оценки студента(0 - закончить ввод):"))
+
+        if grds == 0:
+            break
+
+        grds_l.append(int(grds))
+
+    if stds == "0":
+        break
+
+    average_scoresum = sum(grds_l) / len(grds_l)
+
+    dict_res[stds] = average_scoresum
+
 
 print("Average student score:", dict_res)
