43428f77241e2d71a98b6dba1dee97310eaa0356
Владимир <vm885032@gmail.com>
2025-01-07 19:16:32 +0300

    Add files via upload

---
+++ generator_assemblies.py
@@ -0,0 +1,13 @@
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+
+first_result = (len(x[0]) - len(x[1]) for x in zip(first, second)
+                if len(x[0]) != len(x[1]))
+
+
+second_result = (len(first[x]) == len(second[x]) for x in range(3))
+
+print(list(first_result))
+print(list(second_result))
