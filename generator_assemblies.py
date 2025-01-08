8e040325e2dbdf779c1fae3b3cf1798cfd7153d4
Владимир <vm885032@gmail.com>
2025-01-08 14:20:57 +0300

    Add files via upload

--- generator_assemblies.py
+++ generator_assemblies.py
@@ -7,7 +7,7 @@ first_result = (len(x[0]) - len(x[1]) for x in zip(first, second)
                 if len(x[0]) != len(x[1]))
 
 
-second_result = (len(first[x]) == len(second[x]) for x in range(3))
+second_result = (len(first[x]) == len(second[x]) for x in range(len(second)))
 
 print(list(first_result))
 print(list(second_result))
