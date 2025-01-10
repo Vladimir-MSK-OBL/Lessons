c6241944e633c7c9931ee20e4fd23e6cdac0265c
Владимир <vm885032@gmail.com>
2025-01-10 21:53:16 +0300

    Update generators.py

--- generators.py
+++ generators.py
@@ -1,8 +1,8 @@
 def all_variants(text):
 
     for symbol in (text[0], text[1],
-                   text[2], text[0: 2],
-                   text[1: 3], text
+                   text[2], text[0:2],
+                   text[1:3], text
                    ):
         yield symbol
 
