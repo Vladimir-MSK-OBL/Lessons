e46f4826cec4b1b14f857ee6e3d8ca662808ced1
Владимир <vm885032@gmail.com>
2025-01-10 21:36:17 +0300

    Add files via upload

---
+++ generators.py
@@ -0,0 +1,14 @@
+def all_variants(text):
+
+    for symbol in (text[0], text[1],
+                   text[2], text[0: 2],
+                   text[1: 3], text
+                   ):
+        yield symbol
+
+
+
+a = all_variants("abc")
+
+for i in a:
+    print(i)
